a179592394cf2106f39050bd9c1fd26c8135e748
Solsantacruz <solsantacruz69@gmail.com>
2023-07-06 01:30:29 +0200

    deploy

--- api/index.js
+++ api/index.js
@@ -21,9 +21,11 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
 
+const PORT = process.env.PORT || 3001;
+
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at', process.env.PORT); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log('%s listening at', PORT); // eslint-disable-line no-console
   });
 });
